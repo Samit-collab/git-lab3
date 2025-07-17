4cd5778632bcea54b9553b6127943a58bde6acc3
Samit <6611130023@mut.ac.th>
2025-07-17 16:53:09 +0700

    comm

--- menu.php
+++ menu.php
@@ -19,4 +19,10 @@ echo "Home";
     echo "Mahanakorn";
     echo "Map";
     echo "About me";
+    echo "About me";
+    echo "Home";
+        echo "Product";
+        echo "Mahanakorn";
+        echo "Map";
+        echo "About me";
 ?>
